571cfe6dc9bd92c3bdcfc5ecb03e641b382cb1b0
Ronn Brashear <ronn.brashear@gmail.com>
2020-04-01 15:31:53 -0700

    Relying on external tda_connect now.

--- tda_delta_roller.py
+++ tda_delta_roller.py
@@ -1,20 +1,8 @@
-import os
 import json
 import arrow
-from dotenv import load_dotenv
 import td_ameritrade_api as td
+from .tda_connect import connect_to_tda
 from .pyjson import PyJSON
-
-
-def connect_to_tda():
-    load_dotenv(verbose=True)   
-    refresh_token = os.environ.get('refresh_token', None)
-    client_id = os.environ.get('consumer_id', None)
-    account_id = os.environ.get('account_id', None)
-    if not (refresh_token and client_id and account_id): 
-        raise Exception("Environment variables not set")
-    client = td.client(refresh_token, client_id, account_id)
-    return client
 
 
 def decode_tda_symbol(td_sym):
